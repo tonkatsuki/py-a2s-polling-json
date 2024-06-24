906f4b0830735778192d9933dc95e8d32cfb4d23
tonkatsuki <6411441+tonkatsuki@users.noreply.github.com>
2024-06-24 00:45:05 -0500

    finalize dockerfile stuff

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from flask_cors import CORS
 import threading
 import server_status
 
-# Configuration variables
 HOST = "0.0.0.0"
 PORT = 5000
 
-# Initialize Flask app
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow CORS for all origins
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-@app.route("/servers", methods=["GET"])
+@app.route("/", methods=["GET"])
 def get_server_info():
     master_title = request.args.get("title", "servers")
     formatted_response = {master_title: server_status.server_info_cache}
@@ -20,8 +18,5 @@ def get_server_info():
 
 
 if __name__ == "__main__":
-    # Start the background thread to update server info every minute
     threading.Thread(target=server_status.update_server_info, daemon=True).start()
-
-    # Run the Flask app
     app.run(host=HOST, port=PORT)
